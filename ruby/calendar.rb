c34d670a9143171d77e5aeee417ae290a858e3f5
na-ah <na.ah.0006@gmail.com>
2024-03-16 17:23:03 +0900

    refactor: calendar.rb

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,55 +1,50 @@
 # frozen_string_literal: true
 
-# -m引数で月が指定できる
-# -m引数が不正な場合はエラーを出す
-# 引数を指定しない場合は、今月、今年のカレンダーを表示
-
 require 'date'
-# 0-6の曜日を返す。日曜日が0。
-base_date = Date.new(Date.today.year, Date.today.month, Date.today.day)
-first_day = Date.new(base_date.year, base_date.month, 1)
-last_day = Date.new(base_date.year, base_date.month, -1)
-month_days = (first_day..last_day)
+require 'optparse'
 
-# 曜日、各日の出力幅を指定
-def day_format(day)
-  day.rjust(3)
+# 引数mを処理
+def parsed_option
+  option = ARGV.getopts('m:')['m']&.to_i
+
+  return nil unless option
+  return option if option && (1..12).include?(option)
+
+  puts "#{option} is neigher a month number (1..12) nor a name"
+  exit
 end
 
-# 初週の空白を出力
-def output_first_week_spaces(first_day)
-  if first_day.wday.zero?
-    print day_format(' ') * 6
-  else
-    print day_format(' ') * (first_day.wday - 1)
+# カレンダーの見出しを表示
+def output_calender_header(first_date)
+  puts "#{first_date.strftime('%B %Y').center(21)} "
+  puts 'Mo Tu We Th Fr Sa Su '
+end
+
+# カレンダーの最初の週の空白の個数
+def first_week_space_count(first_date)
+  first_date.wday.zero? ? 6 : first_date.wday - 1
+end
+
+# カレンダーの日付を表示
+def output_days(first_date, last_date)
+  # 最初の週の空白を表示
+  print ' '.rjust(3) * first_week_space_count(first_date)
+
+  # 初日から最終日までを表示
+  (first_date..last_date).each do |date|
+    print "#{date.day.to_s.rjust(2)} "
+    # 日曜日の場合改行
+    print "\n" if date.sunday?
   end
 end
 
-# 月の各日を出力
-def output_days(month_days)
-  month_days.each do |day|
-    # wdayが0(日曜)の時は、改行する
-    if day.wday.zero?
-      print "#{day_format(day.day.to_s)}\n"
-    else
-      print day_format(day.day.to_s)
-    end
-  end
+# カレンダーの出力
+def print_calendar
+  first_date = Date.new(Date.today.year, parsed_option || Date.today.month, 1)
+  last_date = first_date.next_month.prev_day
+
+  output_calender_header(first_date)
+  output_days(first_date, last_date)
 end
 
-# カレンダーの上部を出力
-def output_calender_top(base_date)
-  calendar_title = "#{base_date.year} #{base_date.strftime('%B')}"
-  calendar_weekday = %w[Mo Tu We Th Fr Sa Su].map { |wday| day_format(wday) }.join
-  print "#{calendar_title.center(20)} \n"
-  print "#{calendar_weekday} \n"
-end
-
-# カレンダー全体を出力
-def output_calendar(base_date, month_days)
-  output_calender_top(base_date)
-  output_first_week_spaces(month_days.first)
-  output_days(month_days)
-end
-
-output_calendar(base_date, month_days)
+print_calendar
